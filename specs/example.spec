a20d94717967c2338c4999c08f3d0004c75a555a
simgecelebi <simgecelebii@gmail.com>
2022-02-01 21:17:56 +0300

    Appium

---
+++ specs/example.spec
@@ -0,0 +1,45 @@
+Specification Heading
+=====================
+
+This is an executable specification file. This file follows markdown syntax.
+Every heading in this file denotes a scenario. Every bulleted point denotes a step.
+
+To execute this specification, run
+	gauge specs
+
+Start
+-------------
+* "1" saniye kadar bekle
+* Anasayfa id "Mağazadan Alışveriş" alanını kontrol et
+* "1" saniye kadar bekle
+* Alışverişe başla butonuna bas
+* "1" saniye kadar bekle
+* Kategoriler butonuna bas
+* "1" saniye kadar bekle
+* Kategoriler sayfası xpath "Kategoriler" alanını kontrol et
+* "1" saniye kadar bekle
+* Kadın butonuna bas
+* "1" saniye kadar bekle
+* Pantolon butonuna bas
+* "1" saniye kadar bekle
+* Sayfayı aşağı kaydır
+* "1" saniye kadar bekle
+* Random ürün seç
+* "1" saniye kadar bekle
+* Ürün detay sayfası xpath "SEPETE EKLE" alanını kontrol et
+* "1" saniye kadar bekle
+* Favorilere ekle butonuna bas
+* "1" saniye kadar bekle
+* Giriş yap sayfası xpath "Parolamı Unuttum" alanını kontrol et
+* "3" saniye kadar bekle
+* E-posta adresi alanına "simge.celebi@testinium.com" değerini yaz
+* "3" saniye kadar bekle
+* Parola alanına "12345*" değerini yaz
+* "3" saniye kadar bekle
+* Geri butonuna bas
+* Geri butonuna bas
+* "3" saniye kadar bekle
+* Random ürün seç
+* "3" saniye kadar bekle
+* Beden seç sepete ekle
+* "3" saniye kadar bekle
---
+++ src/test/java/StepImplementation.java
@@ -0,0 +1,87 @@
+import com.google.common.collect.ImmutableMap;
+import com.thoughtworks.gauge.Step;
+import com.thoughtworks.gauge.Table;
+import com.thoughtworks.gauge.TableRow;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.logging.LogEntries;
+
+import java.time.Duration;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+
+public class StepImplementation extends BaseTest {
+
+    @Step("<time> saniye kadar bekle")
+    public void waitForseconds(int time) throws InterruptedException {
+        Thread.sleep(time * 1000);
+    }
+
+    @Step("id <id> li elemente tıkla")
+    public void clickByid(String id) {
+        appiumDriver.findElement(By.id(id)).click();
+    }
+
+    @Step("xpath <xpath> li elemente tıkla")
+    public void clickByxpath(String xpath) {
+        appiumDriver.findElement(By.xpath(xpath)).click();
+    }
+
+    @Step("id <id> li elementi bul ve <text> alanını kontrol et")
+    public void textAreacontrolId(String id, String text) {
+        Assert.assertTrue("Element text değerini içermiyor", appiumDriver.findElement(By.id(id)).getText().contains(text));
+    }
+
+    @Step("Xpath <xpath> li elementi bul ve <text> alanını kontrol et")
+    public void textAreacontrolXpath(String xpath, String text) {
+        Assert.assertTrue("Element text değerini içermiyor", appiumDriver.findElement(By.xpath(xpath)).getText().contains(text));
+    }
+
+    @Step("Sayfayı aşağı kaydır")
+    public void swipeDown() {
+        final int ANIMATION_TIME = 300; // ms
+
+        final int PRESS_TIME = 300; // ms
+
+        int edgeBorder = 10; // better avoid edges
+        PointOption pointOptionStart, pointOptionEnd;
+        // init screen variables
+        Dimension dims = appiumDriver.manage().window().getSize();
+        // init start point = center of screen
+        pointOptionStart = PointOption.point(dims.width / 2, dims.height / 2);
+        pointOptionEnd = PointOption.point(dims.width / 2, edgeBorder);
+        for (int i = 0; i < 2; i++) {
+            new TouchAction(appiumDriver)
+                    .press(pointOptionStart)
+                    // a bit more reliable when we add small wait
+                    .waitAction(WaitOptions.waitOptions(Duration.ofMillis(PRESS_TIME)))
+                    .moveTo(pointOptionEnd)
+                    .release().perform();
+        }
+    }
+
+    @Step("Elementler <xpath> arasından rasgele bir tanesini seç ve tıkla")
+    public void clickRandomelement(String xpath){
+        Random random = new Random();
+        List<MobileElement> products = appiumDriver.findElements(By.xpath(xpath));
+        int index = random.nextInt(products.size());
+        products.get(index).click();
+    }
+
+    @Step("id <id> li elementi bul ve <text> değerini yaz")
+    public void sendkeysByid(String id,String text){
+        appiumDriver.findElement(By.id(id)).sendKeys(text);
+    }
+}
